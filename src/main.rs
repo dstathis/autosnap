021bd42da7eaedd407f64ecaaa5995a03079f391
Dylan Stephano-Shachter <dylan@theone.ninja>
2019-06-25 00:10:21 -0400

    A main loop with connections. Still learning rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+extern crate clap;
+extern crate toml;
+
+use std::fs;
+use std::process::exit;
+use std::{thread, time};
+use clap::{Arg, App, ArgMatches};
+use toml::Value;
+
+mod snaps;
+
+static ONE_MINUTE: u64 = 60;
+static ONE_HOUR: u64 = 60 * 60;
+
+fn parse_args() -> ArgMatches<'static> {
+    let args = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(Arg::with_name("config")
+             .long("config")
+             .takes_value(true)
+             .help("Use this config file"))
+        .get_matches();
+    return args;
+}
+
+fn read_config(config: &str) -> Value {
+    let contents = fs::read_to_string(config);
+    let config_str = match contents {
+        Ok(x)   => x,
+        Err(x)  => {
+            println!("Could not open file {}: {}", config, x);
+            exit(1);
+        }
+    };
+    let conf = config_str.parse::<Value>().unwrap();
+    return conf;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = parse_args();
+    let conf = read_config(args.value_of("config").unwrap_or("/etc/autosnap.toml"));
+    println!("{}", conf["main"]["uris"].as_str().expect("config has no value for hosts"));
+
+    let interval = time::Duration::from_secs(1);
+    loop {
+        let start = time::Instant::now();
+        snaps::do_snaps(&conf);
+        thread::sleep(interval - start.elapsed());
+    }
 }
---
+++ src/snaps.rs
@@ -0,0 +1,32 @@
+extern crate virt;
+
+use toml::Value;
+use virt::connect::Connect;
+
+pub fn do_snaps(conf: &Value) {
+    let flags = virt::connect::VIR_CONNECT_LIST_DOMAINS_ACTIVE |
+                virt::connect::VIR_CONNECT_LIST_DOMAINS_INACTIVE;
+    let uris = conf["main"]["uris"].as_str().expect("No configured URIs").split_whitespace();
+    for uri in uris {
+         let conn = match Connect::open(&uri) {
+             Ok(c)  => c,
+             Err(e) => {
+                 panic!("No connection to hypervisor: code {}, message: {}",
+                        e.code,
+                        e.message);
+             }
+         };
+         println!("{}:", uri);
+         let doms = match conn.list_all_domains(flags) {
+             Ok(c)  => c,
+             Err(e) => {
+                 panic!("Can't get domains from hypervisor: code {}, message: {}",
+                        e.code,
+                        e.message);
+             }
+         };
+         for dom in doms {
+             println!("{}", dom.get_name().unwrap());
+         }
+    }
+}
